90424aea8f6d31cf5a7bddfed1a48d34f9d24bd7
Andrew Pospisil <44903633+anpospisil@users.noreply.github.com>
2020-07-10 12:13:28 +0200

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || "https://quick-bike-server.herokuapp.com/"
+export const apiUrl = process.env.API_URL || "https://quick-bike-server.herokuapp.com"
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
